b5c531c2089e8788ea6ed3ab591f8c733748ff8e
Wagner Carvalho <wagncarv@gmail.com>
2021-03-23 18:00:58 -0300

    testes item e order

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,6 +1,8 @@
 defmodule Exlivery.Factory do
   use ExMachina
+  alias Exlivery.Orders.Item
   alias Exlivery.Users.User
+
 
   def user_factory do
     %User{
@@ -11,5 +13,13 @@ defmodule Exlivery.Factory do
       age: 30
     }
   end
-end
 
+  def item_factory() do
+    %Item{
+      description: "Pizza de peperoni",
+      category: :pizza,
+      quantity: 1,
+      unity_price: Decimal.new("35.5")
+    }
+  end
+end
